f5f201112c26b87ad657a32ba8c620461b739fe0
DenisNikiti <den.hikitin@gmail.com>
2022-12-30 18:12:34 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 
-import Profile from "./components/Profile/Profile"
-import data from "./path/user.json"
-import Staticis from "./path/statistic.json"
-import Table from "./components/Table/Table"
-import Friends from "./path/friends.json"
-import Friendlis from "./components/FriendList/FriendList"
-import Transactions from "./path/transactions.json"
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory"
-
 function App() {
   return (
     <div>
@@ -16,30 +7,7 @@ function App() {
 
        
 
-       <Profile
-      avatar={data.avatar}
-      name={data.username}
-      tag={data.tag}
-      location={data.location}
-        stats={data.stats} />
-      
-      
-
-      <Table
-        items={Staticis}
-        title={Staticis.title}
-        
-
-      />
-      
-      <Friendlis
-        data={Friends}
-
-      />
-    
-      <TransactionHistory
-         history = {Transactions}
-      />
+  
 
 
     </div>
